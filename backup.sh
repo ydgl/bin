7d97ab40fe5f1f2ac628fab5dd5ce5ff4c54e9eb
Denis-Gilles Laurent <denisgilles.laurent@gmail.com>
2014-09-21 21:48:18 +0200

    first fix

--- backup.sh
+++ backup.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
+
+# mount directory
 OSENV=`uname`
 SRCDIR="$HOME/tmp/src"
 DSTDIR="$HOME/tmp/dst"
 
 
-echo create $SRCDIR
+echo create mount dir ...
 mkdir -p $SRCDIR
-
-echo create $DSTDIR
-mkdir -p $SRCDIR
+mkdir -p $DSTDIR
 
 # locale required to be set correctly :
 # export LC_ALL=fr_FR.UTF-8  
 # export LANG=fr_FR.UTF-8
 
+echo mounting dir ...
 if [ "$OSENV" = "Darwin" ]; then
   mount -t smbfs //guest:@mafreebox.free.fr/PLASTIK_III/ $SRCDIR
 else
@@ -23,14 +24,23 @@ fi
 
 sudo mount -t nfs -o proto=tcp,port=2049 nas-adg.local:/nfs/Backup $DSTDIR
 
+# waiting for mount OK
+echo waiting for mount ...
+sleep 10
+
+rsync -a --progress --stats --delete $SRCDIR/DOC $DSTDIR
+#rsync -a --progress --stats --delete $SRCDIR $DSTDIR
 #rsync -a --progress --stats --delete $SRCDIR $DSTDIR
 
+echo unmounting ...
 sudo umount $SRCDIR
 sudo umount $DSTDIR
 
-sleep 30
+echo waiting for unmount ...
+sleep 10
 
+echo cleaning ...
 rmdir $DSTDIR
 rmdir $SRCDIR
-rmdir ~/tmp
+rmdir "$HOME/tmp"
 
