c36b6db7633d909baf8e0c63f0d8c55f7dcc2b27
admin <admin@mac-adg.local>
2016-01-21 22:07:34 +0100

    finalisation de la version

--- backup_Darwin.sh
+++ backup_Darwin.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
+# Use zsh to support multidimensionnal arrays on mac os x 10.8 (bash 3 only)
 
 
 BACKUP_TMP_WORKDIR=$HOME/_running_backup
-SRCDIR="$BACKUP_TMP_WORKDIR/src"
-#DSTDIR="$BACKUP_TMP_WORKDIR/dst"
-DSTDIR="/Volumes/PLASTIK_IV"
-SRC_MOUNT_COMMAND="mount -t smbfs //guest:@mafreebox.free.fr/PLASTIK_III/"
-DST_MOUNT_COMMAND="mount -t smbfs //guest:@nas-adg.local/Backup"
 
+SRCDIR="$BACKUP_TMP_WORKDIR/src"
+SRC_MOUNT_COMMAND="mount -t smbfs //guest:@mafreebox.free.fr/PLASTIK_III/"
+
+DSTDIR="/Volumes/PLASTIK_IV"
+#DSTDIR="$BACKUP_TMP_WORKDIR/dst"
+#DST_MOUNT_COMMAND="mount -t smbfs //guest:@nas-adg.local/Backup"
 
 PATH=$PATH:/sbin
 
@@ -16,14 +18,13 @@ PATH=$PATH:/sbin
 
 # $1 : mount command
 # $2 : mount point
-function trymount {
+function trymount() {
   retval=0
   iMount=1
 
-
   mkdir -p $2
-
-  while [ $iMount -le 2 ] && [ `ls $2 | wc -l` == 0 ]
+  
+  while [ $iMount -le 2 ] && [ `ls $2 | wc -l` -eq 0 ]
   do
     echo $iMount try $2
     $1 $2
@@ -41,7 +42,7 @@ function trymount {
 }
 
 
-function tryumount {
+function tryumount() {
     umount $1
     echo "waiting for unmount ..."
     sleep 10
@@ -68,7 +69,7 @@ echo "start batch at `date`"
 if [ ! -e "$BACKUP_TMP_WORKDIR" ]
 then
     mkdir -p $BACKUP_TMP_WORKDIR
-    #if [ $? -ne 0 ] then exit fi
+    #if [ $? -ne 0 ] then exit fi;
 else
     echo "Backup is already running"
     exit
@@ -82,23 +83,18 @@ s=$?
 #trymount "$DST_MOUNT_COMMAND" "$DSTDIR"
 d=$?
 
+function mysync() {
+    echo "sync $1 > $2"
+    rsync -a --progress --stats --delete "$1" "$2"
+}
 
 if [ $(( d + s )) -eq 0 ]
 then
   echo "start sync at `date`"
-  echo Sync $SRCDIR/DOC/Family
-  rsync -a --progress --stats --delete $SRCDIR/DOC/Family $DSTDIR/DOC
-  #echo Sync $SRCDIR/DOC/DG
-  #rsync -a --progress --stats --delete $SRCDIR/DOC/DG $DSTDIR/DOC
-  #echo Sync $SRCDIR/DOC/Arabelle
-  #rsync -a --progress --stats --delete $SRCDIR/DOC/Arabelle $DSTDIR/DOC
-  #echo Sync $SRCDIR/DOC/Partage
-  #rsync -a --progress --stats --delete $SRCDIR/DOC/Partage $DSTDIR/DOC
-  #echo Sync $SRCDIR/DOC/Partage_ext
-  #rsync -a --progress --stats --delete $SRCDIR/DOC/Partage_ext $DSTDIR/DOC
-  #echo Sync $SRCDIR/FILMS/1-DESSINS ANIMES
-  #rsync -a --progress --stats --delete $SRCDIR/FILMS/1-DESSINS_ANIMES $DSTDIR/FILMS
-  #echo "end sync at `date`"
+  mysync "$SRCDIR/DOC/Family" "$DSTDIR/DOC"
+  mysync "$SRCDIR/DOC/Arabelle" "$DSTDIR/DOC"
+  mysync "$SRCDIR/DOC/FILMS/1-DESSSINS_ANIMES" "$DSTDIR/FILMS"
+  echo "end sync at `date`"
 else
   echo "No sync : fail to mount one of the two directories"
 fi
@@ -111,4 +107,3 @@ tryumount $SRCDIR
 rmdir $BACKUP_TMP_WORKDIR
 
 echo  end batch at `date`
-
