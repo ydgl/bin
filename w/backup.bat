a866ae7404642c3f7ec7e116e5fbba417a5a89b3
dlaurent <dlaurent@apec.fr>
2018-12-14 10:33:42 +0100

    small improvement on backup

--- w/backup.bat
+++ w/backup.bat
@@ -1,9 +1,43 @@
-rem md "U:\20180802 - VPM2 - AAAAMMDD"
-rem robocopy "D:\ApecBox\Espace Personnel\dlaurent (spsnas)\20180802 - VPM2 - AAAAMMDD" "U:\20180802 - VPM2 - AAAAMMDD" /MIR /dcopy:T
-set time1=%date:~6,4%/%date:~3,2%/%date:~0,2%-%time:~0,8%
+@echo off
+
+rem usage : bat dir_to_backup src_home_dir dst_home_dir
+rem ex    : bat "20180514 - Affinite Recruteurs 2018 - AAAAMMDD" "D:\ApecBox\Espace Personnel\dlaurent (spsnas)\" "U:\"
+
+REM TO INSTALL
+REM   1-OPEN WINDOWS TASK PLANNIFIER
+REM   2-CREATE FOLDER FOR YOUR OWN TASK ON THE LEFT
+REM   3-CLICK CREATE TASK AND FOLLOW WIZARD
+
+rem becareful with trailing space !!!
+set dirtobackup=%~1
+set srchomedir=%~2
+set dsthomedir=%~3
+
+rem call function
+call :backupDIR
+
+goto :end
+
+
+
+REM backupDIR backup directory %dirtobackup%
+:backupDIR 
+
+@echo BGN %srchomedir%%dirtobackup% to %dsthomedir%%dirtobackup%
+
+
+md "%dsthomedir%%dirtobackup%"
+
+
+robocopy "%srchomedir%%dirtobackup%" "%dsthomedir%%dirtobackup%" /MIR
+set time1=%date:~6,4%-%date:~3,2%-%date:~0,2%_%time:~0,8%
 set time2=%time1: =0%
+set time2=%time1::=-%
+echo %time2% > "%dsthomedir%%dirtobackup%\%time2%.txt"
 
-echo %time2%
-REM time2 = date time formated as "YYYY/MM/DD-HH:MI:SS"
+echo END %srchomedir%%dirtobackup% to %dsthomedir%
+rem return from function
+goto :eof
 
-md "U:\20180802 - VPM2 - AAAAMMDD"
+
+:end
