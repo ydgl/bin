3dde49f04a0163e4b0b3eb4380172b2a9f03cb9c
dlaurent <dlaurent@apec.fr>
2019-06-18 17:00:59 +0200

    ajout des sous dossiers dans le backup

--- w/backup.bat
+++ w/backup.bat
@@ -1,7 +1,10 @@
 @echo off
 
 rem usage : bat dir_to_backup src_home_dir dst_home_dir
-rem ex    : bat "20180514 - Affinite Recruteurs 2018 - AAAAMMDD" "D:\ApecBox\Espace Personnel\dlaurent (spsnas)\" "U:\"
+rem ex    : bat "my local work directory to backup" "D:\my local work home folder" "U:\my backups"
+rem copy dir_to_backup to dst_home_dir/lbk (shortcut for last backup)
+rem   and
+rem copy changed files in dir_to_backup since 2 days to dst_home_dir/<current day number> (which perform a monthly rolling backup)
 
 REM TO INSTALL
 REM   1-OPEN WINDOWS TASK PLANNIFIER
@@ -15,30 +18,31 @@ set dsthomedir=%~3
 set today=%date:~0,2%
 
 
-rem call function
 call :backupDIR
 
 goto :end
 
 
 
-REM backupDIR backup directory %dirtobackup%
 :backupDIR 
 
-@echo BGN %srchomedir%%dirtobackup% to %dsthomedir%%dirtobackup%
+@echo BGN backupDIR %srchomedir%%dirtobackup% to %dsthomedir%
 
 
+md "%dsthomedir%%dirtobackup%\lbk"
+robocopy "%srchomedir%%dirtobackup%" "%dsthomedir%%dirtobackup%\lbk" /MIR
+
+
+del /FS "%dsthomedir%%dirtobackup%\%today%"
 md "%dsthomedir%%dirtobackup%\%today%"
+robocopy "%srchomedir%%dirtobackup%" "%dsthomedir%%dirtobackup%\%today%" /S /MAXAGE:2
 
-
-robocopy "%srchomedir%%dirtobackup%" "%dsthomedir%%dirtobackup%\%today%" /MIR
 set time1=%date:~6,4%-%date:~3,2%-%date:~0,2%_%time:~0,8%
 set time2=%time1: =0%
 set time2=%time1::=-%
 echo %time2% > "%dsthomedir%%dirtobackup%\%time2%.txt"
 
-echo END %srchomedir%%dirtobackup% to %dsthomedir%
-rem return from function
+@echo END backupDIR %srchomedir%%dirtobackup% to %dsthomedir%
 goto :eof
 
 
