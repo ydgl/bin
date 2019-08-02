8e0e7da00bee2e7c137e17e13fbb4716dcd99f12
dlaurent <dlaurent@apec.fr>
2019-08-02 17:31:32 +0200

    fix

--- w/backup.bat
+++ w/backup.bat
@@ -1,20 +1,25 @@
 @echo off
 
 rem usage : bat dir_to_backup src_home_dir dst_home_dir
-rem ex    : bat "my local work directory to backup" "D:\my local work home folder" "U:\my backups"
-rem copy dir_to_backup to dst_home_dir/lbk (shortcut for last backup)
+rem ex    : bat "my local work directory to backup" "D:\my local work home folder" "U:\my backups" [ibk]
+rem copy dir_to_backup to dst_home_dir/dst_home_dir.bk (shortcut for last backup)
 rem   and
-rem copy changed files in dir_to_backup since 2 days to dst_home_dir/<current day number> (which perform a monthly rolling backup)
+rem copy changed files in dir_to_backup since 2 days to dst_home_dir/dst_home_dir.ibk/<current day number> (which perform a monthly rolling backup)
+REM IF 4th parameter = "ibk"
 
 REM TO INSTALL
 REM   1-OPEN WINDOWS TASK PLANNIFIER
 REM   2-CREATE FOLDER FOR YOUR OWN TASK ON THE LEFT
 REM   3-CLICK CREATE TASK AND FOLLOW WIZARD
 
+REM net use Z: \\spsnas\prj_dgl
+REM net use Z: \\spsnas\prj_dgl /PERSISTENT:YES
+
 rem becareful with trailing space !!!
 set dirtobackup=%~1
 set srchomedir=%~2
 set dsthomedir=%~3
+set inc_bk=%~4
 set today=%date:~0,2%
 
 
@@ -28,19 +33,20 @@ goto :end
 
 @echo BGN backupDIR %srchomedir%%dirtobackup% to %dsthomedir%
 
+if "%inc_bk%" EQU "ibk" (
+  del /FS "%dsthomedir%%dirtobackup%.ibk\%today%"
+  md "%dsthomedir%%dirtobackup%.ibk\%today%"
+  robocopy "%srchomedir%%dirtobackup%" "%dsthomedir%%dirtobackup%.ibk\%today%" /S /MAXAGE:2
+)
 
-md "%dsthomedir%%dirtobackup%\lbk"
-robocopy "%srchomedir%%dirtobackup%" "%dsthomedir%%dirtobackup%\lbk" /MIR
 
-
-del /FS "%dsthomedir%%dirtobackup%\%today%"
-md "%dsthomedir%%dirtobackup%\%today%"
-robocopy "%srchomedir%%dirtobackup%" "%dsthomedir%%dirtobackup%\%today%" /S /MAXAGE:2
+md "%dsthomedir%%dirtobackup%.bk"
+robocopy "%srchomedir%%dirtobackup%" "%dsthomedir%%dirtobackup%.bk" /MIR
 
 set time1=%date:~6,4%-%date:~3,2%-%date:~0,2%_%time:~0,8%
 set time2=%time1: =0%
 set time2=%time1::=-%
-echo %time2% > "%dsthomedir%%dirtobackup%\%time2%.txt"
+echo %time2% > "%dsthomedir%%dirtobackup%.bk\%time2%.txt"
 
 @echo END backupDIR %srchomedir%%dirtobackup% to %dsthomedir%
 goto :eof
