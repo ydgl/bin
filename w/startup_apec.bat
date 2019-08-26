0e2197fa45a5d3c3fb751eca9c6634c0760bda3c
dlaurent <dlaurent@apec.fr>
2019-08-26 17:13:43 +0200

    add startup file

---
+++ w/startup_apec.bat
@@ -0,0 +1,10 @@
+@echo off
+
+REM installation
+REM 1- Clic on windows start menu
+REM 2- Enter "shell:startup", folder startup will open in an explorer window
+REM 3- Create a link to this file
+REM done
+
+net use Z: \\spsnas\prj_dgl /PERSISTENT:YES
+
